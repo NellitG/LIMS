a240f728d75feea0cf4f42e786720484be4066a6
NellitG <kamaunelson157@gmail.com>
2026-01-20 09:05:01 +0300

    Create laboratory app, register and create models

---
+++ laboratories/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Laboratory(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
